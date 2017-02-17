2b9c3461269b93cf089262bbf18a857d112cfecf
TehesFR <ths.arborisoft@gmail.com>
2017-02-17 22:48:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# About this repo
+
+Phpmyadmin image, based on nazarpc/phpmyadmin.
+
+# Docker-compose
+
+Please check a complete docker-compose.yml example at: https://github.com/TehesFR/docker-apache-php
+
+[Docker Hub page](https://hub.docker.com/r/tehes/docker-phpmyadmin/)
---
+++ config.inc.php
@@ -0,0 +1,35 @@
+<?php
+include 'config.sample.inc.php';
+if (!isset($_ENV['MYSQL_HOST']) && isset($_ENV['MYSQL_PORT_3306_TCP_ADDR'])) {
+	$_ENV['MYSQL_HOST'] = $_ENV['MYSQL_PORT_3306_TCP_ADDR'];
+}
+$hosts = isset($_ENV['MYSQL_HOST']) ? $_ENV['MYSQL_HOST'] : 'mysql';
+foreach (explode(',', $hosts) as $index => $host) {
+	$config = &$cfg['Servers'][$index + 1];
+	$host   = trim($host);
+	if (strpos($host, ':') !== false) {
+		list($host, $port) = explode(':', $host);
+		$config['port'] = $port;
+	}
+	$config['host']            = $host;
+	$config['AllowNoPassword'] = true;
+}
+if (isset($_ENV['ALLOW_ARBITRARY'])) {
+	$cfg['AllowArbitraryServer'] = (bool)$_ENV['ALLOW_ARBITRARY'];
+}
+if (isset($_ENV['ABSOLUTE_URI'])) {
+	$cfg['PmaAbsoluteUri'] = $_ENV['ABSOLUTE_URI'];
+}
+if (isset($_ENV['SESSION_TIMEOUT'])) {
+	$cfg['LoginCookieValidity'] = $_ENV['SESSION_TIMEOUT'];
+	ini_set('session.gc_maxlifetime', $_ENV['SESSION_TIMEOUT']);
+}
+$file_with_secret = 'config.inc.secret.php';
+if (!file_exists($file_with_secret)) {
+	$secret = hash('sha512', openssl_random_pseudo_bytes(1000));
+	file_put_contents(
+		$file_with_secret,
+		"<?php \$cfg['blowfish_secret'] = '$secret';"
+	);
+}
+include $file_with_secret;
